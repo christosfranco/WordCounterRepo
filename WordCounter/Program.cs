d540a67f68e2163114bbc875402b5652361264df
Christian Franck <arboefranck@gmail.com>
2024-04-20 01:40:52 +0200

    remove unused packages

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-
-
-class Program
+﻿class Program
 {
     static async Task Main(string[] args)
     {
